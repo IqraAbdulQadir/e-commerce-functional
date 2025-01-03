062e71c2e368ff4136539c0b364b66bcf2132091
Your New GitHub Username <your-new-email@example.com>
2025-01-04 00:06:54 +0500

    fixed params error

--- src/app/Products/[slug]/page.tsx
+++ src/app/Products/[slug]/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { getProductsBySlug } from '@/sanity/lib/sanityCode';
 import { urlFor } from '@/sanity/lib/image';
 import ProductDetail from './ProductDetail'; // Import the client component
@@ -5,6 +7,8 @@ import ProductDetail from './ProductDetail'; // Import the client component
 export const dynamic = 'force-dynamic'; // Ensures dynamic fetching
 
 const ProductDetailPage = async ({ params }: { params: { slug: string } }) => {
+  'use params';
+
   const product = await getProductsBySlug(params.slug);
 
   if (!product) return <p>Product not found!</p>;
@@ -12,6 +16,7 @@ const ProductDetailPage = async ({ params }: { params: { slug: string } }) => {
   return (
     <ProductDetail
       product={{
+        id: product.slug.current, // Add the id property
         name: product.name,
         price: product.price,
         description: product.description,
